a130a8d95ec629acfd1f87a537af800b5d51709b
Greg <projectyuka86@gmail.com>
2025-04-30 11:48:18 -0400

    final commit

---
+++ rng-dice-script.js
@@ -0,0 +1,32 @@
+// <!-- 🎲 -->
+// <!-- 🔢 -->
+
+const scoreTracker = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+};
+
+
+// roll the dice to get number random number from 1 to 6
+function rollDice() {
+    const roll = Math.floor(Math.random() * 6) + 1;
+    scoreTracker[roll]++; // this increases count of rolled numbers in scoreTracker
+    console.log(`🎲 You rolled a ${roll}`);
+}
+
+function displayScores() {
+    console.log(`🔢 Dice Roll Score Tracker:`);
+    for (const roll in scoreTracker) {
+        console.log(`${roll}: ${scoreTracker[roll]} times`);
+    }
+}
+
+for (let i = 0; i < 100; i++) {
+    rollDice();
+}
+
+displayScores();
